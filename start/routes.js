650ea54481e725b790caf2599bc7cae215e6a8c2
Radi <radi.rusadi.s@gmail.com>
2019-02-27 08:30:15 +0700

    Configure env

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,7 @@
-'use strict'
+'use strict';
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
 Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+  return { greeting: 'Hello world in JSON' };
+});
